44d2118a72fa643124a7dcd1fd2e25b488ee5f46
mastergray <me@mastergray.com>
2023-11-11 10:44:27 -0600

    Updated package.json and prod.js config

--- configs/prod.js
+++ configs/prod.js
@@ -3,12 +3,12 @@ const path = require('path');
 // Configures webpack for creating PROD build of yngwie.js:
 module.exports = {
   mode:"production",
-  entry: './src/main.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'yngwie.js',
+    filename: 'image-beam-cannon.js',
     library:{
-      name:"Yngwie",
+      name:"ImageBeamCannon",
       type:"umd"
     }
   }
